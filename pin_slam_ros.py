3fbc59f703f3de9e81ae41127f1cb1a9721f2edb
kelly7707 <zhoujiawei678@gmail.com>
2024-10-31 14:27:49 +0100

    katzensee_d

--- pin_slam_ros.py
+++ pin_slam_ros.py
@@ -679,23 +679,27 @@ class PINSLAMer:
 if __name__ == "__main__":
 
     config_path = rospy.get_param('~config_path', "./config/lidar_slam/run_ncd_128.yaml")
-    # -- New College Dataset
-    point_cloud_topic = rospy.get_param('~point_cloud_topic', "/os_cloud_node/points")
-    imu_topic = rospy.get_param('~imu_topic', "/os_cloud_node/imu")
-    ts_field_name = rospy.get_param('~point_timestamp_field_name', "t")
-
-    # bag_path = 'data/Newer_College_Dataset/2021-07-01-10-37-38-quad-easy.bag'
-    # bag_path = 'data/Newer_College_Dataset/medium/2021-07-01-11-31-35_0-quad-medium.bag'
-    # bag_path = 'data/Newer_College_Dataset/math_easy/2021-04-07-13-52-31_1-math-easy.bag'
-    bag_path = 'data/Newer_College_Dataset/mine_easy/2021-04-12-11-11-33-mine_medium.bag'
-    
-    # # -- ASL
-    # point_cloud_topic = rospy.get_param('~point_cloud_topic', "/ouster/points")
-    # imu_topic = rospy.get_param('~imu_topic', "/ouster/imu")
+    # # -- New College Dataset
+    # point_cloud_topic = rospy.get_param('~point_cloud_topic', "/os_cloud_node/points")
+    # imu_topic = rospy.get_param('~imu_topic', "/os_cloud_node/imu")
     # ts_field_name = rospy.get_param('~point_timestamp_field_name', "t")
-    # # bag_path = 'data/ASL/field_s/2023-08-09-19-05-05-field_s.bag'
-    # bag_path = 'data/ASL/katzensee/2023-08-21-10-20-22-katzensee_s.bag'
+
+    # # bag_path = 'data/Newer_College_Dataset/2021-07-01-10-37-38-quad-easy.bag'
+    # # bag_path = 'data/Newer_College_Dataset/medium/2021-07-01-11-31-35_0-quad-medium.bag'
+    # # bag_path = 'data/Newer_College_Dataset/math_easy/2021-04-07-13-52-31_1-math-easy.bag'
+    # # bag_path = 'data/Newer_College_Dataset/mine_easy/2021-04-12-11-11-33-mine_medium.bag'
     
+
+
+    # -- ASL
+    point_cloud_topic = rospy.get_param('~point_cloud_topic', "/ouster/points")
+    imu_topic = rospy.get_param('~imu_topic', "/ouster/imu")
+    ts_field_name = rospy.get_param('~point_timestamp_field_name', "t")
+    # bag_path = 'data/ASL/field_s/2023-08-09-19-05-05-field_s.bag'
+    # bag_path = 'data/ASL/katzensee/2023-08-21-10-20-22-katzensee_s.bag'
+    bag_path = 'data/ASL/katzensee_d/2023-08-21-10-29-20-katzensee_d.bag'
+
+
     # If you would like to directly run the python script without including it in a ROS package
     # python pin_slam_ros_node.py [path_to_your_config_file] [point_cloud_topic]
     print("If you would like to directly run the python script without including it in a ROS package\n\
